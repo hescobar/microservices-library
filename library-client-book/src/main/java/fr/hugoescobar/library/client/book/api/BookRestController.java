30dec7a0adf571688ec261f5bf677862ada7f47e
hescobar <kironoka@hotmail.com>
2018-09-05 15:59:38 +0200

    re

--- library-client-book/src/main/java/fr/hugoescobar/library/client/book/api/BookRestController.java
+++ library-client-book/src/main/java/fr/hugoescobar/library/client/book/api/BookRestController.java
@@ -17,45 +17,38 @@ public class BookRestController {
 	@Autowired
 	BookRepository repository;
 
-	@RequestMapping("/save")
-	public String process() {
-		// save a single Customer
-		repository.save(new Book("Fondation", "Isaac Asimov", Genre.SCIFI));
-
-		// save a list of Customers
-		repository.saveAll(Arrays.asList(new Book("Dune", "Frank Herbert", Genre.SCIFI),
-				new Book("Hamlet", "William Shakespeare", Genre.DRAMA),
+	@RequestMapping("/init")
+	public String init() {
+		repository.saveAll(Arrays.asList(new Book("Fondation", "Isaac Asimov", Genre.SCIFI),
+				new Book("Dune", "Frank Herbert", Genre.SCIFI), new Book("Hamlet", "William Shakespeare", Genre.DRAMA),
 				new Book("Fifty Shades of Grey", "E. L. James", Genre.ROMANCE),
 				new Book("Da Vinci Code", "Dan Brown", Genre.THRILLER)));
 
-		return "Done";
+		return "Database initialized";
 	}
-	
-	@RequestMapping("/findall")
-	public String findAll(){
+
+	@RequestMapping("/all")
+	public String findAll() {
 		String result = "";
-		
-		for(Book book : repository.findAll()){
+		for (Book book : repository.findAll()) {
 			result += book.toString() + "<br>";
 		}
 		return result;
 	}
-	
-	@RequestMapping("/findbyid")
-	public String findById(@RequestParam("id") long id){
+
+	@RequestMapping("/find")
+	public String findById(@RequestParam("id") long id) {
 		String result = "";
 		result = repository.findById(id).toString();
 		return result;
 	}
-	
-	@RequestMapping("/findbyname")
-	public String fetchDataByLastName(@RequestParam("name") String name){
+
+	@RequestMapping("/find")
+	public String findByName(@RequestParam("name") String name) {
 		String result = "";
-		
-		for(Book book: repository.findByName(name)){
-			result += book.toString() + "<br>"; 
+		for (Book book : repository.findByName(name)) {
+			result += book.toString() + "<br>";
 		}
-		
 		return result;
 	}
 
